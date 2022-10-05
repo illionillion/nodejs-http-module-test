abaae2361f4b51a1d780875699b0b6738fb26f9c
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-05 13:18:32 +0900

    試しにpostを受け取る練習をしてみた。もう少し考え直してやってみるのもありかも

--- server.js
+++ server.js
@@ -1,16 +1,49 @@
-const http = require('http')
+import { createServer } from 'http';
+import { networkInterfaces } from 'os';
+import { readFileSync } from 'fs';
+
+// get ip address
+const nets = networkInterfaces();
+const net = nets["en0"]?.find((v) => v.family == "IPv4");
+const ipAddress = net.address
+
 const port = 3000;
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {
-        "Content-Type": "text/html"
-      });
+const server = createServer((request, response) => {
+
+  // url取得
+  console.log(request.url)
+  // メソッド
+  console.log(request.method)
+  // console.log(request.headers)
+
+  response.writeHead(200, {
+    "Content-Type": "text/html"
+  });
+
+  if (request.method === 'GET') {
+    
+    const html = readFileSync('./html/index.html')
   
-      const responseMessage = "<h1>Hello World</h1>";
-      response.end(responseMessage);
-      console.log(`Sent a response : ${responseMessage}`);
+    response.write(html)
+    response.end();
+  } else if (request.method === 'POST') {
+
+    let data
+    request.on('data', function(chunk) {data += chunk})
+      .on('end', function() {
+ 
+        console.log(data);
+        response.end(data);
+ 
+    })
+
+    // response.write('POST')
+    // response.end()
+  }
 
 });
 
 server.listen(port)
+console.log('http://' + ipAddress + ":" + port);
 console.log('http://localhost:' + port);
