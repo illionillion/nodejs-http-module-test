f51696d24b5a7c522a20b692d56f48974b5f95a2
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-09 21:41:32 +0900

    秒カウント追加

--- test1.js
+++ test1.js
@@ -7,20 +7,28 @@ const timer = s => {
 }
 
 const main = async () => {
+    
+    const startTime = new Date()
+    const timerId = setInterval(() => {
+        console.log(new Date() - startTime);
+    }, 1000);
 
-    
     let flag = false
-    
+
     setTimeout(() => {
+        // 5秒後にflagをtrue
         flag = true
+        console.log('setTimeout ran');
     }, 5000);
 
-    // 変数に特定の値が入るまでループして待つ
+    // flagがtrueになるまでループして待つ
     while (!flag) {
-        console.log('flagはfalse');
+        console.log('flag is false');
         await timer(100)
     }
-    console.log('flagはtrue');
+    console.log('flag is true');
+
+    clearInterval(timerId)
 
 }
 
